14e9f3e137029225baaf7d7f18a65f12f90e49f4
daflockinger <florian.mitterbauer@gmx.net>
2019-06-20 18:59:19 +0200

    created project base

---
+++ src/main/java/com/flockinger/kryptovoyant/scraper/KryptovoyantScraperApplication.java
@@ -0,0 +1,13 @@
+package com.flockinger.kryptovoyant.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryptovoyantScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KryptovoyantScraperApplication.class, args);
+	}
+
+}
